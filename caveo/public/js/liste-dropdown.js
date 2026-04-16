cf5e395da9d6c6d4b5c686549f8a700b53bf1817
DreamCurson <dreamcurson@outlook.com>
2026-04-16 10:29:04 -0400

    Transformation du script du dropdown pour sauvegarder dans la session l'état

--- caveo/public/js/liste-dropdown.js
+++ caveo/public/js/liste-dropdown.js
@@ -1,8 +1,23 @@
 document.querySelectorAll(".toggle-liste").forEach((el) => {
+    const targetId = el.dataset.target;
+    const target = document.getElementById(targetId);
+
+    // Retrouve si c'était ouvert ou fermer lors de la dernière visualisation de la page
+    const isOpen = sessionStorage.getItem(targetId) === "open";
+    if (isOpen) {
+        target.classList.remove("hidden");
+    }
+
     el.addEventListener("click", (e) => {
         if (e.target.closest("a, button, form")) return;
 
-        const target = document.getElementById(el.dataset.target);
         target.classList.toggle("hidden");
+
+        // Sauvegarde temporairement si ouvert ou fermer
+        if (target.classList.contains("hidden")) {
+            sessionStorage.setItem(targetId, "closed");
+        } else {
+            sessionStorage.setItem(targetId, "open");
+        }
     });
 });
