f5586233d68400fbacdc47f8b9ae19f974013333
Greszta <00.Patrik.Greszta@gmail.com>
2026-04-16 14:38:22 -0400

    Ajout des fonction pour modifier password et supprimer compte

--- caveo/app/Http/Controllers/UtilisateurController.php
+++ caveo/app/Http/Controllers/UtilisateurController.php
@@ -50,7 +50,43 @@ class UtilisateurController extends Controller
             'email' => $request->email,
         ]);
 
-        return redirect()->route('profil.show')->with('status', 'Informations mises à jour !');
+        return redirect()->route('profil.show')->with('success', 'Informations mises à jour!');
+    }
+
+    /**
+     * Affiche le formulaire de modification du mot de passe.
+     */
+    public function editPassword()
+    {
+        return view('profil.edit-password');
+
+    }
+
+    /**
+     * Met à jour le mot de passe de l'utilisateur.
+     */
+    public function updatePassword(Request $request)
+    {
+
+        $utilisateur = Auth::user();
+
+        if (!Hash::check($request->ancien_mot_de_passe, $utilisateur->mot_de_passe)) {
+            return back()->withErrors(['ancien_mot_de_passe' => 'Mot de passe incorrect']);
+        }
+
+        $request->validate([
+            'nouveau_mot_de_passe' => 'required|min:8',
+            'nouveau_mot_de_passe_confirmation' => 'required|same:nouveau_mot_de_passe',
+        ], [
+            'nouveau_mot_de_passe.min' => 'Minimum 8 caractères',
+            'nouveau_mot_de_passe_confirmation.same' => 'Les mots de passe ne correspondent pas',
+        ]);
+
+        $utilisateur->mot_de_passe = Hash::make($request->nouveau_mot_de_passe);
+        $utilisateur->save();
+
+
+        return redirect()->route('profil.edit')->with('success', 'Mot de passe modifié!');
     }
 
     /**
@@ -58,6 +94,10 @@ class UtilisateurController extends Controller
      */
     public function destroy(Utilisateur $utilisateur)
     {
-        //
+        $utilisateur = Auth::user();
+
+        $utilisateur->delete();
+
+        return redirect('/')->with('success', 'Votre compte a été supprimé.');
     }
 }
